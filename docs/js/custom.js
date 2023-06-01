8c99734544038d66618fff8a0675e0079e3bc4b0
Ludaro <waschmaschvanlu@gmail.com>
2023-06-01 02:00:28 +0200

    Update custom.js

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,11 +1,1 @@
-if (window.location.pathname === 'mkdocs-Ludaro-Wiki') {
-    fetch('https://ipapi.co/json/')
-      .then(response => response.json())
-      .then(data => {
-        if (data.country === 'DE') {
-          window.location.href = '/mkdocs-Ludaro-Wiki/deutsch/willkommen/';
-        } else {
-          window.location.href = '/mkdocs-Ludaro-Wiki/englisch/welcome';
-        }
-      });
-  }
+
